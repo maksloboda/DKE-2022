e3426a3af2feab0e2b94e7dd2e6a769bc93aa6b1
Seq <peogne@gmail.com>
2022-12-14 18:31:38 +0400

    Add route for get_count_per_user

--- shop/app/urls.py
+++ shop/app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='home'),
-    path('count', views.get_count_client, name='count')
+    path('count', views.get_count_client, name='count'),
+    path('get_count_per_user', views.get_count_per_user, name='count_per_user')
 ]
--- shop/app/views.py
+++ shop/app/views.py
@@ -21,4 +21,17 @@ def get_count_client(request):
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
+def get_count_per_user(request):
+    cursor = connection.cursor()
+    cursor.execute('''SELECT users.*, orders.order_count
+    from
+    (
+        SELECT "Код клиента", count(distinct "Код заказа") as order_count
+        from "Заказ клиента" group by "Код клиента"
+    ) as orders
+    join Клиент as users on orders."Код клиента" == users."Код клиента"''')
+    result = cursor.fetchall()
 
+    response_data = [{"Код клиента": obj[0], "Фамилия":obj[1], "Имя":obj[2], "Город":obj[3], "Код заказа":obj[4], "Количество ордеров":obj[5]} for obj in result]
+
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
